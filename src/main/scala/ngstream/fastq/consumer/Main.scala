1132f87b653dde41a3b5199d6b7ad7a2e34a9dfe
pjvanthof <pjrvanthof@gmail.com>
2019-07-03 17:43:08 +0200

    basic consumer

---
+++ src/main/scala/ngstream/fastq/consumer/Main.scala
@@ -0,0 +1,55 @@
+package ngstream.fastq.consumer
+
+import java.io.File
+import java.time.Duration
+import java.util.Properties
+
+import ngstream.fastq.schema.{Fastq, FastqSerde}
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import scopt.{OParser, OParserBuilder}
+
+import scala.collection.JavaConverters.asJavaCollection
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val cmdArgs = OParser.parse(argParser, args, CmdArgs())
+      .getOrElse(throw new IllegalArgumentException())
+
+    val props = new Properties()
+    props.put("bootstrap.servers", cmdArgs.brokers.mkString(","))
+    props.put("key.deserializer", classOf[StringDeserializer].getCanonicalName)
+    props.put("value.deserializer", classOf[FastqSerde].getCanonicalName)
+    props.put("group.id", "group2")
+
+    val consumer = new KafkaConsumer[String, Fastq](props)
+
+    consumer.subscribe(asJavaCollection(List(cmdArgs.topic)))
+
+    while (true) {
+      val bla = consumer.poll(Duration.ofSeconds(5))
+      bla.iterator().forEachRemaining(x => println(x))
+    }
+  }
+
+  case class CmdArgs(r1: File = null,
+                     r2: Option[File] = None,
+                     brokers: Seq[String] = Seq(),
+                     topic: String = null)
+
+  def argParser: OParser[Unit, CmdArgs] = {
+    val builder: OParserBuilder[CmdArgs] = OParser.builder[CmdArgs]
+    import builder._
+    OParser.sequence(
+      head("Ngstream fastq kafka producer"),
+      opt[String]("broker")
+        .action((x, c) => c.copy(brokers = c.brokers :+ x))
+        .required()
+        .text("Kafka broker, can supply multiple"),
+      opt[String]("topic")
+        .action((x, c) => c.copy(topic = x))
+        .required()
+        .text("Kafka topic")
+    )
+  }
+}
